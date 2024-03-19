846cf841c71cf55d35b236f273aa64d92c9d718d
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-03-19 13:54:07 -0300

    Update app.component.ts

--- login-app/src/app/app.component.ts
+++ login-app/src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBars,
+  faLock,
+  faUserCircle,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'login-app';
+  title = 'loginPage';
+  public faUserCircle = faUserCircle;
+  public faBars = faBars;
+  public faLock = faLock;
 }
+
